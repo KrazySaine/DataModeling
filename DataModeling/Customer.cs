ef1039a32c508ad19b65c790688dc8665438bbb6
christopher cane <christopher.cane@student.centralia.edu>
2024-10-28 11:33:19 -0700

    Add project files.

---
+++ DataModeling/Customer.cs
@@ -0,0 +1,36 @@
+﻿namespace DataModeling
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public ICollection<PhoneNumbers> PhoneNumbers { get; set; }
+        public ICollection<Addresses> Addresses { get; set; }
+        public ICollection<Likes> Likes { get; set; }
+    }
+    public class PhoneNumbers
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+    public class Addresses
+    {
+        public int Id { get; set; }
+        public Customer Customer { set; get; }
+        public string Address { get; set; }
+    }
+    public class Likes
+    {
+        public int Id { get; set; }
+        public Customer Customer{ get; set; }
+        public string Like {  get; set; }
+    }
+
+
+
+}
