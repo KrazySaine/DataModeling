199badc8fcf6b712664f365c2d6c805f423ef7c2
christopher cane <christopher.cane@student.centralia.edu>
2024-11-04 11:11:17 -0800

    Finalized

--- DataModeling/Customer.cs
+++ DataModeling/Customer.cs
@@ -1,34 +1,42 @@
-﻿namespace DataModeling
+﻿using DataModeling;
+
+namespace DataModeling
 {
     public class Customer
     {
-        public int Id { get; set; }
+        public int CustomerId { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string BirthDate { get; set; }
         public string EmailAddress { get; set; }
-        public ICollection<PhoneNumbers> PhoneNumbers { get; set; }
-        public ICollection<Addresses> Addresses { get; set; }
+        public int PhoneID { get; set; }
+        public string AddressID { get; set; }
+        public string LikesID { get; set; }
+
+        public Addresses Addresses { get; set; }
         public ICollection<Likes> Likes { get; set; }
     }
     public class PhoneNumbers //one to many
     {
-        public int Id { get; set; }
-        public Customer Customer { get; set; }
+        public int PhoneId { get; set; }
+        
         public string PhoneNumber { get; set; }
-    }
+
+        public ICollection<Customer> { get; set; }
+
+}
     public class Addresses //many to one
     {
-        public int Id { get; set; }
+        public int AddressId { get; set; }
         public Customer Customer { set; get; }
         public string Address { get; set; }
     }
     public class Likes //many to many
     {
-        public int Id { get; set; }
-        public Customer Customer{ get; set; }
+        public int LikesId { get; set; }
         public string Like {  get; set; }
+        public ICollection <Customer> Customers { get; set; }
     }
 
 
